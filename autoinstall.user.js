29cae6bbae0c830b95620053a6f8de7d900a3718
Vinylgeorge <cpartha2009@hotmail.com>
2025-09-23 21:11:33 +0530

    Update autoinstall.user.js

--- autoinstall.user.js
+++ autoinstall.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Install MTurk Scripts Bundle
 // @namespace   Violentmonkey Scripts
-// @version     1.0
+// @version     1.1
 // @description Auto-open both MTurk scripts for install in same time
 // @match       *://*/*
 // @grant       none
@@ -12,9 +12,9 @@
   'use strict';
 
   const scripts = [
-    "https://raw.githubusercontent.com/Vinylgeorge/Team_Mani/refs/heads/main/Mturk_tasks.user.js",
-    "https://raw.githubusercontent.com/Vinylgeorge/Team_Mani/refs/heads/main/queuerrv.user.js",
-    "https://raw.githubusercontent.com/Vinylgeorge/Team_Mani/refs/heads/main/refresh.user.js"
+    "chrome-extension://jinjaccalgkegednnccohejagnlnfdag/confirm/index.html#VMrkuyecrcoz",
+    "chrome-extension://jinjaccalgkegednnccohejagnlnfdag/confirm/index.html#VM83agjshiu6n",
+    "chrome-extension://jinjaccalgkegednnccohejagnlnfdag/confirm/index.html#VM5js3zva1ylf"
   ];
 
   // Open each script in new tab once
